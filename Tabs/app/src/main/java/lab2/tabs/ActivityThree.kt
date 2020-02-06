aa92e210fa71a5cef58d4f714e9b272bd63165ac
AleksKirienko <aleks.Kirienko@bk.ru>
2020-02-07 00:55:32 +0700

    Add ListView is catNames

---
+++ Tabs/app/src/main/java/lab2/tabs/ActivityThree.kt
@@ -0,0 +1,24 @@
+package lab2.tabs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_three.*
+
+class ActivityThree : AppCompatActivity() {
+
+    val catNames = arrayOf(
+        "Рыжик", "Барсик", "Муся", "Васька",
+        "Пушок", "Кузьмич", "Шерхан"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, catNames)
+
+        // устанавливаем адаптер списку
+        listView.adapter = adapter
+    }
+}
--- Tabs/app/src/main/java/lab2/tabs/MainActivity.kt
+++ Tabs/app/src/main/java/lab2/tabs/MainActivity.kt
@@ -4,16 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var openButton: Button? = null
+    private var listButton: Button? = null
+    private var lastButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
     }
 
-    fun loadImage(view: View) {
-        val openAct = Intent(this, activityTwo::class.java)
-        startActivity(openAct)
+    private fun initView() {
+        openButton = findViewById(R.id.button1)
+        listButton = findViewById(R.id.button2)
+        lastButton = findViewById(R.id.button3)
+
+        openButton!!.setOnClickListener {
+            //startActivityForResult(Intent(this, activityTwo::class.java), ACTIVITY_TWO_REQUEST_CODE)
+            val openAct = Intent(this, activityTwo::class.java)
+            startActivity(openAct)
+        }
+
+        listButton!!.setOnClickListener {
+            val list = Intent(this, ActivityThree::class.java)
+            startActivity(list)
+        }
     }
 }
