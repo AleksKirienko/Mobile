24620617f0e2f8346f3e0a9d45bd32e9001a248d
AleksKirienko <aleks.Kirienko@bk.ru>
2020-11-11 02:56:40 +0700

    add comments

--- SolarSystem/app/src/main/java/ru/sibsutis/solarsystem/Planet.java
+++ SolarSystem/app/src/main/java/ru/sibsutis/solarsystem/Planet.java
@@ -16,12 +16,11 @@ public class Planet implements GLSurfaceView.Renderer {
     public FloatBuffer textureBuffer;
 
     public Planet(float R) {
-        float i = 0;
         n = 0;
         int dtheta = 15, dphi = 15;
         float DTOR = (float) (Math.PI / 180.0f);
-        ByteBuffer byteBuf = ByteBuffer.allocateDirect(5000 * 3 * 4);
-        byteBuf.order(ByteOrder.nativeOrder());
+        ByteBuffer byteBuf = ByteBuffer.allocateDirect(5000 * 3 * 4);  // выделение памяти из основной кучи JVM
+        byteBuf.order(ByteOrder.nativeOrder()); // извлекает собственный порядок байтов базовой платформы
         mVertexBuffer = byteBuf.asFloatBuffer();
         byteBuf = ByteBuffer.allocateDirect(5000 * 2 * 4);
         byteBuf.order(ByteOrder.nativeOrder());
@@ -74,11 +73,11 @@ public class Planet implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        gl.glEnable(GL10.GL_BLEND); //разрешить наложение цветов
-        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA); //алгоритм смешения
+        gl.glEnable(GL10.GL_BLEND); // разрешение на наложение цветов
+        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA); //алгоритм смешения с масштабными коэффициентами
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY); // разрешить массив вершин
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
-        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer); // определяет массив данных вершин, хранит в памяти видеокарты
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);   // c текстурами
 
         for (int i = 0; i < n; i += 4)
             gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, i, 4);
--- SolarSystem/app/src/main/java/ru/sibsutis/solarsystem/SolarSystemRenderer.java
+++ SolarSystem/app/src/main/java/ru/sibsutis/solarsystem/SolarSystemRenderer.java
@@ -34,12 +34,12 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
     private void loadGLTexture(GL10 gl) {
         gl.glGenTextures(3, textures, 0);
         for (int i = 0; i < texture_name.length; ++i) {
-            gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[i]);
+            gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[i]); // привязка текстуры
             gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
             InputStream is = c.getResources().openRawResource(texture_name[i]);
-            Bitmap bitmap = BitmapFactory.decodeStream(is);
-            GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
-            bitmap.recycle();
+            Bitmap bitmap = BitmapFactory.decodeStream(is);  //создание объекта bitmap
+            GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);  // загрузка текстуры
+            bitmap.recycle();  // используется для освобождения памяти
         }
     }
 
@@ -47,10 +47,10 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         gl.glClearColor(0.0f, 0.0f, 0, 1.0f);
         gl.glClearDepthf(1);
-        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glEnable(GL10.GL_DEPTH_TEST); // разрешение глубины
         gl.glMatrixMode(GL10.GL_PROJECTION);
-        gl.glLoadIdentity();
-        gl.glOrthof(-10, 10, -10, 10, -10, 10);
+        gl.glLoadIdentity();  // Матрица идентичности сбрасывает матрицу обратно в состояние по умолчанию
+        gl.glOrthof(-10, 10, -10, 10, -10, 10); // применяет орфографическую проекцию
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
         gl.glScalef(1, 0.60f, 1);
@@ -69,48 +69,50 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
         p = (p == 360) ? 0 : p + 2;
         angle = (angle == 360) ? 0 : angle + 0.15f;
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-        gl.glEnable(GL10.GL_TEXTURE_2D);
-        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
-        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, Sun.textureBuffer);
-        gl.glPushMatrix();
+        gl.glEnable(GL10.GL_TEXTURE_2D);  // подключение текстур
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);  // привязка
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY); // устанавливают состояние клиентской части
+
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, Sun.textureBuffer); // загрузка текстуры
+        gl.glPushMatrix();  // копирует верхнюю матрицу и помещает ее в стек
         gl.glRotatef(90, 1, 0, 0);
         gl.glRotatef(p, 0, 0, 0.1f);
         gl.glColor4f(1, 1, 0, 1);
         Sun.onDrawFrame(gl);
-        gl.glPopMatrix();
-        RotationOffset = 6.0f;
-        RotationSpeed = 0.05f;
-        gl.glPushMatrix();
+        gl.glPopMatrix();  // извлекает верхнюю матрицу из стека
+        RotationOffset = 6.0f;  // смещение вращения
+        RotationSpeed = 0.05f;  // скорость вращения
+        gl.glPushMatrix();  // копирует верхнюю матрицу и помещает ее в стек
 
-        gl.glTranslatef(RotationOffset * (float) (cos(angle * RotationSpeed)), 0,
+        gl.glTranslatef(RotationOffset * (float) (cos(angle * RotationSpeed)), 0f,
                 RotationOffset * (float) (sin(angle * RotationSpeed)));
 
         gl.glRotatef(90, 1, 0, 0);
         gl.glRotatef(p, 0, 0, 2);
         gl.glPushMatrix();
+
+        gl.glEnable(GL10.GL_TEXTURE_2D);  // подключение текстуры
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[1]);
         gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, Earth.textureBuffer);
         gl.glColor4f(1, 1, 1, 1);
-
         Earth.onDrawFrame(gl);
         gl.glRotatef(-p, 0.3f, 1, 0);
         RotationOffset = 1.5f;
         RotationSpeed = 0.2f;
 
-        gl.glTranslatef(RotationOffset * (float) (cos(1 * RotationSpeed)), 0,
+        gl.glTranslatef(RotationOffset * (float) (cos(1 * RotationSpeed)), 0f,
                 RotationOffset * (float) (sin(1 * RotationSpeed)));
 
-        gl.glRotatef(p, 0, 1, 0);
-        gl.glColor4f(0, 0, 1f, 1);
+        gl.glEnable(GL10.GL_TEXTURE_2D);  // подключение текстуры
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[2]);
         gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, Moon.textureBuffer);
         gl.glColor4f(1, 1, 1, 1);
-
         Moon.onDrawFrame(gl);
+        gl.glTranslatef(0.3f, 0f, 1f);
+
         gl.glPopMatrix();
         gl.glPopMatrix();
         gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-        gl.glDisable(GL10.GL_TEXTURE_2D);
+        gl.glDisable(GL10.GL_TEXTURE_2D); // отключ
     }
 }
