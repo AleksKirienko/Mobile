16e16306507354b5c9f5e0bde6825be0fd130067
AleksKirienko <aleks.Kirienko@bk.ru>
2020-11-12 18:47:01 +0700

    First phong

---
+++ Phong/app/src/main/java/ru/sibsutis/phong/MainActivity.java
@@ -0,0 +1,46 @@
+package ru.sibsutis.phong;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private GLSurfaceView myGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!supportES2()) {
+            Toast.makeText(this, "OpenGl ES 2.0 is not supported", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        myGLSurfaceView = new GLSurfaceView(this);
+        myGLSurfaceView.setEGLContextClientVersion(2);
+        myGLSurfaceView.setRenderer(new sphereRenderer(this));
+        setContentView(myGLSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myGLSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myGLSurfaceView.onResume();
+    }
+
+    private boolean supportES2() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        return (configurationInfo.reqGlEsVersion >= 0x20000);
+    }
+}
